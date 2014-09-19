4c5b58f180164ded5e559eeeffe7b7751610c038
Nelson Riley <nelsonriley@gmail.com>
2014-09-19 13:30:45 -0700

    code cleanup and comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,18 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+/*
+Story in a few words:
+
+return an array of nodes in the dom
+traverse the entire document with a depth first search
+see the code
+      
+*/
+
 var getElementsByClassName = function(className){
   var elements = [];
 
-  var dive = function(node) {
+  var traverse = function(node) {
     if (node.nodeType === 1 && node.classList.contains(className)) {
       elements.push(node);
     }
     _.each(node.childNodes, function(child) {
-      dive(child);
+      traverse(child);
     });
-  }
-  dive(document.body);
+  };
+  traverse(document.body);
   
   return elements;
 };
